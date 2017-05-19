d9d46834f9e2e2792dd1c7ab1fb2b0078193b74c
Neil Marlowe <nmarlowe@gmail.com>
2017-05-18 22:09:04 -0400

    Change to GET all cards in list

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
+// var apiSettings = {
+//     "async": true,
+//     "crossDomain": true,
+//     "url": "https://api.trello.com/1/cards/59166dc10cf965066a06bae8",
+//     "type": "GET"
+// };
+
 var apiSettings = {
     "async": true,
     "crossDomain": true,
-    "url": "https://api.trello.com/1/cards/59166dc10cf965066a06bae8",
+    "url": "https://api.trello.com/1/lists/59166dbbb5adda80877f2257/cards",
     "type": "GET"
 };
 
@@ -11,10 +18,12 @@ function getCard() {
     var desc = "";
     var label = "";
 
-    name = response.name;
-    desc = response.desc;
-    label = response.labels[0].color;
+    //TODO: Loop through each response array item
+    name = response[1].name;
+    desc = response[1].desc;
+    label = response[1].labels[0].color;
 
+    console.log(response.length);
     console.log("Name: " + name);
     console.log("Description: " + desc);
     console.log("Color: " + label);
